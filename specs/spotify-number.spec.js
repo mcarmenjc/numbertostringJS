f9762b4dbff3e91a21f42c034ec26013f9bf50c4
MCarmen Jimenez Campos <matrimc@gmail.com>
2016-03-01 23:28:36 +0000

    added implementation of to string of number class

--- specs/spotify-number.spec.js
+++ specs/spotify-number.spec.js
@@ -41,6 +41,41 @@ describe('Spotify.Number toString', function(){
 
 	it ('should return three hundred for 300 value', function(){
 		number.value = 300;
-		expect(number.toString()).toEqual('three hudred');
+		expect(number.toString()).toEqual('three hundred');
+	});
+
+	it ('should return one thousand two hundred and thirty four for 1234 value', function(){
+		number.value = 1234;
+		expect(number.toString()).toEqual('one thousand, two hundred and thirty four');
+	});
+
+	it ('should return five thousand for 5000 value', function(){
+		number.value = 5000;
+		expect(number.toString()).toEqual('five thousand');
+	});
+
+	it ('should return ninety nine for 99 value', function(){
+		number.value = 99;
+		expect(number.toString()).toEqual('ninety nine');
+	});
+
+	it ('should return three hundred and ten for 310 value', function(){
+		number.value = 310;
+		expect(number.toString()).toEqual('three hundred and ten');
+	});
+
+	it ('should return one thousand, five hundred and one for 1501 value', function(){
+		number.value = 1501;
+		expect(number.toString()).toEqual('one thousand, five hundred and one');
+	});
+
+	it ('should return twelve thousand, six hundred and nine for 12609 value', function(){
+		number.value = 12609;
+		expect(number.toString()).toEqual('twelve thousand, six hundred and nine');
+	});
+
+	it ('should return five hundred and twelve thousand, six hundred and seven for 512607 value', function(){
+		number.value = 512607;
+		expect(number.toString()).toEqual('five hundred and twelve thousand, six hundred and seven');
 	});
 });
--- src/spotify-number.js
+++ src/spotify-number.js
@@ -4,10 +4,79 @@ Spotify.Number = function(value) {
   this.value = value;
 };
 
-/**
- * @method toString
- * @public
- */
 Spotify.Number.prototype.toString = function() {
-	return '';
+	var numbersMap = {
+			units: {
+				0: 'zero',
+				1: 'one',
+				2: 'two',
+				3: 'three',
+				4: 'four',
+				5: 'five',
+				6: 'six',
+				7: 'seven',
+				8: 'eight',
+				9: 'nine'
+			},
+			tens: {
+				10: 'ten',
+				11: 'eleven',
+				12: 'twelve',
+				13: 'thirteen',
+				14: 'fourteen',
+				15: 'fifteen',
+				16: 'sixteen',
+				17: 'seventeen',
+				18: 'eighteen',
+				19: 'nineteen',
+				2: 'twenty',
+				3: 'thirty',
+				4: 'fourty',
+				5: 'fifty',
+				6: 'sixty',
+				7: 'seventy',
+				8: 'eighty',
+				9: 'ninety'
+			}
+		},
+		numberString = '';
+
+	function printUnitAndTens(tens){
+		var tensString = '';
+		if (tens < 10){
+			return numbersMap.units[tens];
+		}
+		if (tens < 20){
+			return numbersMap.tens[tens];
+		}
+		tensString = numbersMap.tens[Math.floor(tens/10)];
+		if (tens%10 !== 0) {
+			tensString = tensString + ' ' + numbersMap.units[tens%10];
+		}
+		return tensString;
+	}
+
+	function printHundreds(hundreds){
+		var hundredsString = '';
+		if (Math.floor(hundreds/100) === 0){
+			return printUnitAndTens(hundreds%100);
+		}
+
+		hundredsString = numbersMap.units[Math.floor(hundreds/100)] + ' hundred';	
+		if (hundreds % 100 !== 0){
+			hundredsString = hundredsString + ' and ' + printUnitAndTens(hundreds%100);
+		}
+		return hundredsString;
+	}
+
+	if (this.value >= 1000){
+		numberString = printHundreds(Math.floor(this.value/1000)) + ' thousand';
+		if (this.value % 1000 !== 0){
+			numberString = numberString + ', ';
+		}
+	}
+	if (this.value % 1000 !== 0){
+		numberString = numberString + printHundreds(this.value%1000);
+	}
+	return numberString;
 };
